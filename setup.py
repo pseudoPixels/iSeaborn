4b9f883ae0f5045327423c7d1856b80500b89397
pseudoPixels <codevision.source@gmail.com>
2019-12-12 19:27:53 -0500

    project init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='bokehBorn',
+      version='0.0.1',
+      description="bokehBorn",
+      author='Golam Mostaeen',
+      author_email='golammostaeen@gmail.com',
+      license='',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['pandas',
+        'bokeh==1.4.0',
+        'numpy']
+      )
