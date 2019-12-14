aa1e09ee821358414b8a834c4d4c90afe3c0b018
pseudoPixels <codevision.source@gmail.com>
2019-12-13 19:57:46 -0500

    notebook examples updated

--- testing/test_bokehBorn/testBarplot.py
+++ testing/test_bokehBorn/testBarplot.py
@@ -3,6 +3,6 @@ from bokehBorn.categorical import barplot
 
 tips = sns.load_dataset("tips")
 
-barplot(x="day", y="total_bill", data=tips, orient="h")
+barplot(x="day", y="total_bill", data=tips)
 
 
