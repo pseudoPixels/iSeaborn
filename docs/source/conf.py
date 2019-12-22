5230752742efd8ef0a4aabbfd0e8403a84c81a40
pseudoPixels <codevision.source@gmail.com>
2019-12-22 02:13:12 -0500

    contents bug fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,11 +63,11 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    #'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': True
 }
