55f948b9946e0f744087b3c793be60de42544028
pseudoPixels <codevision.source@gmail.com>
2019-12-22 02:09:39 -0500

    html theme options added 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx_rtd_theme']
 
-
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['../_templates']
